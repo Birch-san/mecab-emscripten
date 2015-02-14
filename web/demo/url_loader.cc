104616bf9667bb9916dda302cdf874cbfe285a79
Alex Birch <birch@Alexs-MacBook-Pro-2.local>
2015-02-14 17:42:39 +0000

    Got it url loading a thing

--- web/demo/url_loader.cc
+++ web/demo/url_loader.cc
@@ -1,40 +1,15 @@
-//  MeCab -- Yet Another Part-of-Speech and Morphological Analyzer
-//
-//
-//  Copyright(C) 2001-2006 Taku Kudo <taku@chasen.org>
-//  Copyright(C) 2004-2006 Nippon Telegraph and Telephone Corporation
-#include "mecab.h"
-#include "winmain.h"
-#include <iostream>
-#include <unistd.h>
-#include <ppapi/cpp/instance.h>
-#include <ppapi/cpp/module.h>
-#include <ppapi/cpp/var.h>
-// #include <ppapi_simple/ps_main.h>
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
-// int main(int argc, char **argv) {
-//   return mecab_do (argc, argv);
-// }
+// This example demonstrates how to load content of the page into NaCl module.
 
-/*int ppapi_simple_main(int argc, char** argv) {
-    for (int i=0; i<argc; ++i) {
-        std::cout << "Argument " << i << ": " << argv[i] << std::endl;
-        // std::cout << mecab_do(argc, &argv[i]);
-    }
-
-	std::cout << "yo" << std::endl;
-    std::cout << mecab_do2(*argv) << std::endl;
-
-    // std::cerr << "Standard error output appears in the debug console\n";
-
-    // Since PNaCl modules aren't supposed to terminate, we need to give
-    // Pepper some breathing room to convey all the messages:
-    sleep(3);
-
-    return 0;
-}
-
-PPAPI_SIMPLE_REGISTER_MAIN(ppapi_simple_main)*/
+#include <cstdio>
+#include <string>
+#include "ppapi/cpp/instance.h"
+#include "ppapi/cpp/module.h"
+#include "ppapi/cpp/url_loader.h"
+#include "ppapi/cpp/var.h"
 
 #include "url_loader_handler.h"
 
