e639074105097578e654b16cb2c4479e807c40f3
Alex Birch <birch@Alexs-MacBook-Pro.local>
2015-04-05 21:21:52 +0100

    Refactor that shi' into a new files

--- src/url_loader.cpp
+++ src/url_loader.cpp
@@ -10,9 +10,6 @@
 #include "winmain.h"
 #include <iostream>
 #include <unistd.h>
-#include <ppapi/cpp/instance.h>
-#include <ppapi/cpp/module.h>
-#include <ppapi/cpp/var.h>
 // #include <ppapi_simple/ps_main.h>
 
 // Global variables
@@ -45,31 +42,7 @@
 
 PPAPI_SIMPLE_REGISTER_MAIN(ppapi_simple_main)*/
 
-#include "url_loader_handler.h"
-
-// These are the method names as JavaScript sees them.
-namespace {
-const char* const kLoadUrlMethodId = "getUrl";
-static const char kMessageArgumentSeparator = ':';
-}  // namespace
-
-class URLLoaderInstance : public pp::Instance {
- public:
-  explicit URLLoaderInstance(PP_Instance instance) : pp::Instance(instance) {}
-  virtual ~URLLoaderInstance() {}
-
-  // Called by the browser to handle the postMessage() call in Javascript.
-  // The message in this case is expected to contain the string 'getUrl'
-  // followed by a ':' separator, then the URL to fetch.  If a valid message
-  // of the form 'getUrl:URL' is received, then start up an asynchronous
-  // download of URL.  In the event that errors occur, this method posts an
-  // error string back to the browser.
-  virtual void HandleMessage(const pp::Var& var_message);
-  using pp::Instance::PostMessage;
-  void PostMessage(const pp::Var& message);
- private:  // Prevents erroneous use by other classes.
-  typedef pp::Instance super;
-};
+#include "url_loader.h"
 
 void URLLoaderInstance::HandleMessage(const pp::Var& var_message) {
   if (!var_message.is_string()) {
@@ -127,18 +100,3 @@ void URLLoaderInstance::PostMessage(const pp::Var& message) {
 
   super::PostMessage(var_result);
 }
-
-class URLLoaderModule : public pp::Module {
- public:
-  URLLoaderModule() : pp::Module() {}
-  virtual ~URLLoaderModule() {}
-
-  // Create and return a URLLoaderInstance object.
-  virtual pp::Instance* CreateInstance(PP_Instance instance) {
-    return new URLLoaderInstance(instance);
-  }
-};
-
-namespace pp {
-Module* CreateModule() { return new URLLoaderModule(); }
-}  // namespace pp
